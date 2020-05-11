ec05f97edcc02149f985ad15e3a50dd70c1877c4
wawanHN <databy2019@gmail.com>
2020-05-10 21:56:34 -0700

    update requirements and settings.py

--- taskmate/settings.py
+++ taskmate/settings.py
@@ -1,3 +1,4 @@
+import django_heroku
 import os
 import environ
 
@@ -122,3 +123,5 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 LOGIN_REDIRECT_URL = 'todolist'
 
 LOGIN_URL = 'login'
+
+django_heroku.settings(locals())
